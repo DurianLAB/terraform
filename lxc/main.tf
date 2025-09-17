d14d151dc9758e2c71d476bb8759994f97d7a83d
An Pham <anpham@example.com>
2025-09-16 18:03:36 -0700

    fix with grok

--- lxc/main.tf
+++ lxc/main.tf
@@ -1,7 +1,7 @@
 # root main.tf
 
 provider "lxd" {
- remote {
+  remote {
     name    = "lxd-server-1"
     address = "https://142.129.233.95:8443"
     token   = var.lxd_auth_token
@@ -11,14 +11,14 @@ provider "lxd" {
 module "k3s_cluster_node" {
   source = "./module/lxc-k3s-vm"
 
-  instance_name    = "my-k3s-node"
-  image_alias      = "ubuntu-daily:22.04"
-  ephemeral        = false
-  network_name     = "k3s-network"
-  ipv4_address     = "10.150.19.1/24"
-  ipv6_address     = "fd42:474b:622d:259d::1/64"
-  host_listen_ip   = "10.150.19.1"
-  storage_pool     = "my-dir-pool"
+  instance_name  = "my-k3s-node"
+  image_alias    = "ubuntu-daily:22.04"
+  ephemeral      = false
+  network_name   = "k3s-network"
+  ipv4_address   = "10.150.19.1/24"
+  ipv6_address   = "fd42:474b:622d:259d::1/64"
+  host_listen_ip = "10.150.19.1"
+  storage_pool   = "my-dir-pool"
 }
 
 output "k3s_node_ip" {
