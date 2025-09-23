adf062ee25ada76aac393e5358c7fa2c3c8e014b
An Pham <anpham@example.com>
2025-09-22 23:08:57 -0700

    update for ssh key adding on instance

--- lxc/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "lxd_auth_token" {
-  description = "The authentication token for the LXD remote host."
-  type        = string
-  sensitive   = true # This prevents the value from being displayed in output
-}
